8bd4880208584666799522063f97ffd7cf14e45b
NotEvanH <as4510723@gmail.com>
2026-04-26 08:53:30 +0800

    Added semblance of documentation to LoadingScreen.py

--- modules/LoadingScreen.py
+++ modules/LoadingScreen.py
@@ -10,35 +10,35 @@ RESET = Colours["Reset"]
 
 COLUMNS, rows = shutil.get_terminal_size()
 rows -= 1
-DROPS = [random.randint(0, rows) for _ in range(COLUMNS)]
 
 CHARACTERS = string.ascii_lowercase + string.ascii_uppercase + string.punctuation
 
 # INTERNAL FUNCTIONS
 
 def _clear() -> None:
-    os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear") # Use an appropriate command to clear the terminal
 
 # EXTERNAL FUNCTIONS
 
 def loading_screen(message: str) -> None:
-    message_x = (COLUMNS - len(message)) // 2
-    message_y = rows // 2
+    message_x = (COLUMNS - len(message)) // 2 # Find where the message should start typing on the x-axis
+    message_y = rows // 2 # Get the middle to type the message
 
     characters_loaded = 0
     
-    for frame in range(40):
-        output = []
+    for frame in range(40): # Forty frames
+        output = [] # Store each line that should be loaded
 
         if frame % 3 == 0 and characters_loaded < len(message):
-            characters_loaded += 1
+            characters_loaded += 1 # Load another character every three frames
         
-        for y in range(rows):
+        for y in range(rows): # Loop through each column
             line = ""
             for x in range(COLUMNS):
                 if y == message_y and message_x <= x < message_x + len(message):
-                    index = x - message_x
+                    index = x - message_x # Figure out where letter should be loaded
 
+                    # Load letter
                     if index < characters_loaded:
                         line += f"{GREEN}{message[index]}{RESET}"
                     else:
@@ -46,11 +46,11 @@ def loading_screen(message: str) -> None:
                 else:
                     line += " "
 
-            output.append(line)
+            output.append(line) # Append new line to output list
 
-        _clear()
-        print("\n".join(output))
-        time.sleep(0.05)
+        _clear() # Clear screen from past frame
+        print("\n".join(output)) # Output stuff
+        time.sleep(0.05) # Short visual pause
 
     output = []
     for y in range(rows):
@@ -63,9 +63,9 @@ def loading_screen(message: str) -> None:
 
         output.append(line)
 
-    _clear()
-    print("\n".join(output))
+    _clear() # Clear screen from past frame
+    print("\n".join(output))  # Print final output
 
-    time.sleep(1)
+    time.sleep(1) # Pause for one second
 
-    _clear()
+    _clear() # Clear screen to load actual menu
